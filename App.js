e279382381cc3a5977f6423f0d05131dbab871c0
ohnas <ohnas12@gmail.com>
2023-05-31 18:26:05 +0900

    Apploading -> SplashScreen

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import AppLoading from "expo-app-loading";
-import React, { useState } from "react";
-import { Text } from "react-native";
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback, useEffect, useState } from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
   const onFinish = () => setReady(true);
-  const startLoading = async () => {};
+  async function prepare() {
+    try {
+      // Pre-load fonts, make any API calls you need to do here
+      // await Font.loadAsync(Entypo.font);
+      // Artificially delay for two seconds to simulate a slow loading
+      // experience. Please remove this if you copy and paste the code!
+      await new Promise(resolve => setTimeout(resolve, 5000));
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      // Tell the application to render
+      setAppIsReady(true);
+    }
+  };
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+  useEffect(() => {
+    prepare();
+  }, [])
 
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  if (!appIsReady) {
+    return null;
   }
-  return <Text>We are done loading!</Text>;
+
+  return (
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}>
+      <Text>SplashScreen Demo! 👋</Text>
+    </View>
+  );
 }
