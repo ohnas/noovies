cefdcda1773d7f5b4a7533488d4be869983f4c56
ohnas <ohnas12@gmail.com>
2023-06-01 20:13:48 +0900

    Typescript

---
+++ styled.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components/native'
+
+
+declare module 'styled-components/native' {
+  export interface DefaultTheme {
+    mainBgColor: string;
+    textColor: string;
+    accentColor: string;
+  }
+}
