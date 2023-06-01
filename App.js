c3b9a54244f0a67dc3c189b80b6b04755efdbe02
ohnas <ohnas12@gmail.com>
2023-06-01 17:46:35 +0900

    Stacks and Tabs

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import * as SplashScreen from 'expo-splash-screen';
 import React, { useCallback, useEffect, useState } from "react";
-import { Text, View } from "react-native";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
 import { NavigationContainer } from "@react-navigation/native";
-import Tabs from "./navigation/Tabs";
-import Stack from "./navigation/Stack";
+import Root from './navigation/Root';
 
 const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
@@ -46,7 +44,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack />
+      <Root />
     </NavigationContainer>
   );
 }
